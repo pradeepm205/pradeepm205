0275177b5bd4155a7ebeb55b547437e33c10c4ec
pradeepm205 <99172149+pradeepm205@users.noreply.github.com>
2022-02-07 17:50:13 +0530

    Add files via upload

---
+++ PrimeNum.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace Assignments
+{
+
+
+    class PrimeNum
+    {
+       static bool isPrime(int n)
+
+        {
+            if (n <= 1)
+                return false;
+            for (int i = 2; i <=n/2; i++)
+
+                if (n % i == 0)
+
+                   return false;
+            return true;
+
+        }
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Enter the number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if(isPrime(n))
+                Console.WriteLine("true");
+            else
+                Console.WriteLine("false");
+        }
+    }
+}
