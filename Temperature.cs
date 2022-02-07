a065487e7678c1ca6a02b300214d3429a4a16d43
pradeepm205 <99172149+pradeepm205@users.noreply.github.com>
2022-02-07 17:53:49 +0530

    Add files via upload

---
+++ Temperature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1Temp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the celsius value : ");
+            int celsius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Kelvin value is = {0}", celsius + 273);
+            Console.WriteLine("fahrenheit value is ={0}", celsius * 18 / 10 + 32);
+
+        }
+    }
+}
